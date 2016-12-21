fe72bdc25fa716e59807a22360f3f6a617b4108d
marorider <mark.holcman@gmail.com>
2016-12-22 00:25:21 +0100

    Create divisors.js

---
+++ divisors.js
@@ -0,0 +1,16 @@
+function divisor() {
+var s;
+var n1 = 
+  document.getElementById("number");
+if (n1!= Number) {
+  alert ("Podaj Liczbę!")
+  }
+ else {
+  for(i=1; i<=n1; i++) {
+    var n2 = n1 % i;
+    if (n2 == 0) {
+      s += i + ", ";
+      }
+  }
+  alert("Dzielniki liczby " + n1 + " to:/n" + s);
+ };
